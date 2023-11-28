fa5d3944577ea24414210c12ee34521a071c2fe1
EchOrdis <yuc096@ucsd.edu>
2023-11-27 16:51:35 -0800

    necessary changes

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,10 +14,30 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
-set -e
 if [[ -f student-submission/Listexamples.java ]]
 then 
     echo 'File existed'
 else
     echo 'Error'
 fi
+
+found=$(find -name "ListExamples.java")
+
+cp ${found} grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp $CPATH *.java 2>&1 | tee compile.txt
+
+if [[ $? -eq 0 ]]
+then
+    echo 'Compile pass'
+else
+    echo 'Error'
+fi
+
+
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples >test_result.txt
